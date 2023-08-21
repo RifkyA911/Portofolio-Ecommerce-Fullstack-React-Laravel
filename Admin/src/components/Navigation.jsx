786be19017af7c69c8d80fc1c17886e8e2b6b4b6
Rifky Akhmad F <rifkyakhmad911@gmail.com>
2023-08-21 21:57:02 +0700

    hhh

---
+++ Admin/src/components/Navigation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dashboard() {
+  // Konten komponen
+  return <>{/* Isi komponen */}</>;
+}
+export default Dashboard;
--- Admin/src/components/Sidebar.jsx
+++ Admin/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const sideNavigation = [
   { name: "Dashboard", href: "/", current: true },
@@ -10,30 +10,68 @@ const sideNavigation = [
   { name: "Settings", href: "/settings", current: false },
 ];
 
-export default function Sidebar() {
+const Sidebar = ({ toggleNavbar }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
-    <div className="flex flex-col p-5 bg-white drop-shadow-md w-80">
-      <div className="space-y-3">
-        <div className="flex items-center">
-          <h2 className="text-l font-bold">Menu</h2>
+    <div className="flex h-screen bg-white">
+      {/* Sidebar */}
+      <div
+        className={`bg-gray-800 text-white w-56 flex-shrink-0 overflow-hidden transition-transform duration-300 ${
+          sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="h-16 flex items-center justify-center">
+          Sidebar Logo
         </div>
-        <div className="flex-1">
-          <ul className="pt-2 pb-4 space-y-1 text-sm">
+        <nav className="flex-1 overflow-hidden">
+          <ul className="py-4 space-y-1 text-left">
             {sideNavigation.map((item) => (
-              <li key={item.name} className="rounded-sm">
+              <li key={item.name}>
                 <a
                   href={item.href}
-                  className={`flex items-center p-2 space-x-3 rounded-md ${
-                    item.current ? "text-black font-semibold" : "text-gray-600"
+                  className={`block pl-4 pr-6 py-2 ${
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700"
                   }`}
                 >
-                  <span>{item.name}</span>
+                  {item.name}
                 </a>
               </li>
             ))}
           </ul>
-        </div>
+        </nav>
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-1">
+        <button
+          className="toggler flex-shrink-0 p-2 text-gray-500"
+          onClick={toggleSidebar}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
-}
+};
+
+export default Sidebar;
