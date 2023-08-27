57c2def4f1ae1cf461e0251c73bb497cf783a42d
merdinabrori <merdin@gmail.com>
2023-08-27 11:16:40 +0700

    menambahkan fungsi create & update pada admin

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
-use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 // use Validator;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -42,7 +43,7 @@ class UserController extends Controller
         if ($validator->fails()) {
             return new PostResource(false, "validasi data error", ['errors'=>$validator->errors(), 'old_input'=>$request->all()]);
         }
-        $addUser = User::create($validator->validated());
+        $addUser = User::create($request->all());
         return new PostResource(true, "User berhasil ditambahkan.", $addUser);
     }
 
@@ -59,31 +60,30 @@ class UserController extends Controller
      */
     public function update(Request $request)
     {
+        $updateUser = User::find($request->input('id'));
         $validator = Validator::make($request->all(), [
-            "email" => 'required|email',
+            "email" => ['required', 'email', Rule::unique('users', 'email')->ignore($request->input('id'))],
             "username" => 'required',
-            "old_password" => 'required|min:6',
-            "password" => 'min:6|confirmed'
+            "password" => 'required|min:6',
+            "new-password" => 'min:6|confirmed'
         ]);
         
         if ($validator->fails()) {
-            return new PostResource(false, "validasi data error", ['errors'=>$validator->errors(), 'old_input'=>$request->all()]);
+            return new PostResource(false, "validasi data error", ['errors'=>$validator->errors(), 'old_input'=>$request->except('id')]);
         }
-        
-        $updateUser = User::find($request->input('id'));
-        
+                        
         // cek password lama
-        if (!Hash::check($request->input('old_password'), $updateUser->password)) {
-            return new PostResource(false, "Password lama salah.", ['old_input'=>$request->all()]);
+        if (!Hash::check($request->input('password'), $updateUser->password)) {
+            return new PostResource(false, "Password lama salah.", ['old_input'=>$request->except('id')]);
         }
 
         // isi data baru
         $updateUser->email = $request->input('email');
         $updateUser->username = $request->input('username');
-        if ($request->input('password') !== null) {
+        if ($request->input('new-password') !== null) {
             $updateUser->password = $request->input('password');
         } else {
-            $updateUser->password = $request->input('old_password');
+            $updateUser->password = $request->input('password');
         }
         $updateUser->address = $request->input('address');
         $updateUser->verified = $request->input('verified');
--- server/app/Models/Admin.php
+++ server/app/Models/Admin.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Admin extends Model
 {
-    use HasFactory;
+    // use HasFactory;
+    protected $fillable = ['email', 'username', 'pw', 'role', 'pict'];
+    protected $casts = [
+        'pw' => 'hashed'
+    ];
 }
