2fc96557e3ec46e1200a35d3b4e29ea97895bbf7
Rifky Akhmad F <rifkyakhmad911@gmail.com>
2023-08-30 22:12:57 +0700

    integrate login api laravel server

--- Admin/src/components/Pages/Login.jsx
+++ Admin/src/components/Pages/Login.jsx
@@ -11,7 +11,7 @@ function Login() {
           <div className="w-96 bg-gradient-to-b from-violet-400 to-blue-400 rounded-xl shadow-sm text-white font-semibold">
             <h1 className="p-4 text-2xl">Login</h1>
             <form
-              action=""
+              action="http://127.0.0.1:8000/api/admins/login"
               method="post"
               className="flex flex-col text-left py-6 justify-center"
             >
@@ -24,6 +24,7 @@ function Login() {
                   name="email"
                   className="peer mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
                   placeholder="you@example.com"
+                  value="haha@gmail.com"
                 />
                 <p className="mt-2 invisible peer-invalid:visible text-pink-600 text-sm">
                   Please provide a valid email address.
@@ -35,11 +36,13 @@ function Login() {
                 </span>
                 <input
                   type="password"
-                  name="password"
+                  name="pw"
                   className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
                   placeholder="your password"
+                  value="cikidaw"
                 />
               </label>
+              <input type="hidden" name="auth_key" id="" value="cikidaw" />
               <button className="flex-none w-48 mx-auto self-center py-2 px-6 text-center bg-blue-500 hover:bg-sky-700 rounded-md">
                 Login
               </button>
