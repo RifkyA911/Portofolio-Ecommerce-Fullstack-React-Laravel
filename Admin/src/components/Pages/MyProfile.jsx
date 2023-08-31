1fa0260f8a899d827abeeb7f35650fc25bce8d47
Rifky Akhmad F <rifkyakhmad911@gmail.com>
2023-08-31 16:53:13 +0700

    layoutting myprofile

--- Admin/src/components/Pages/MyProfile.jsx
+++ Admin/src/components/Pages/MyProfile.jsx
@@ -3,25 +3,30 @@ import axios from "axios";
 
 export default function MyProfile() {
   const [formData, setFormData] = useState({
-    id: 1,
-    email: "super.duper@gmail.com",
-    username: "Palalo sini gw genjreng",
-    password: "superadmin",
+    id: null,
+    email: "",
+    username: "",
+    pict: "",
   });
-
-  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/admins/1")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data); // Cek data untuk pemeriksaan
-        setUsers(data.users); // Menggunakan data.Users karena 'Users' adalah properti dalam objek
+        // console.log(data.data.id); // Cek data untuk pemeriksaan
+        setFormData((prevFormData) => ({
+          ...prevFormData, //spread opreator object
+          id: data.data.id,
+          email: data.data.email,
+          username: data.data.username,
+          pict: data.data.pict,
+        }));
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }, []);
+  // console.log(formData); // Ini mungkin akan menunjukkan perubahan pada state, tetapi mungkin juga belum terpantau perubahannya.
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -47,72 +52,90 @@ export default function MyProfile() {
 
   return (
     <>
-      {/* baris-1 */}
-      <div className="font-bold justify-center lg:max-h-64">
-        <div className="flex flex-wrap lg:flex-nowrap flex-row">
-          <div className="flex flex-col lg:flex-row lg:justify-center py-4 lg:py-5 px-3 lg:px-4 w-full ">
-            <ul className="flex w-full bg-white">
-              <li className="shadow-md w-96">
-                <img
-                  src="./src/assets/user_avatar/84719630_p1.jpg"
-                  className="w-60 h-60 "
-                />
-              </li>
-              <li></li>
-            </ul>
-            <ul className="flex w-full bg-white">
-              <form className="font-base justify-start" onSubmit={handleSubmit}>
-                <input type="hidden" name="id" value={formData.id} />
-                <label className="block text-left">
-                  <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                    Email
-                  </span>
-                  <input
-                    type="email"
-                    name="email"
-                    className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                    value={formData.email}
-                    onChange={handleChange}
-                    placeholder="Email"
-                  />
-                </label>
-                <label className="block text-left">
-                  <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                    Username
-                  </span>
-                  <input
-                    type="username"
-                    name="username"
-                    className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                    value={formData.username}
-                    onChange={handleChange}
-                    placeholder="Username"
-                  />
-                </label>
-                <label className="block text-left">
-                  <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
-                    Username
-                  </span>
-                  <input
-                    className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                    type="password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    placeholder="Password"
-                  />
-                </label>
-                <button className="btn btn-warning mt-4" type="submit">
-                  Update Admin
-                </button>
-              </form>
-            </ul>
+      {!formData.id ? (
+        <div>
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <div className="MyProfile">
+          <div className="font-bold justify-center lg:max-h-full">
+            <div className="flex flex-wrap lg:flex-nowrap flex-row">
+              <div className="flex flex-col lg:flex-row lg:justify-center py-4 lg:py-5 px-3 lg:px-4 w-full ">
+                <ul className="flex flex-wrap w-full bg-white justify-center items-center">
+                  <form
+                    className="font-base flex flex-row justify-start"
+                    onSubmit={handleSubmit}
+                  >
+                    <li className="flex flex-1 w-64 justify-center items-center">
+                      <img
+                        src={`./src/assets/admin_avatar/${formData.pict}`}
+                        className="w-60 h-60 mr-24"
+                      />
+                    </li>
+                    <li className="flex flex-col flex-1 w-full">
+                      <div className="flex flex-row">
+                        <label className="mb-4 pr-4 block text-left">
+                          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                            Email
+                          </span>
+                          <input
+                            type="email"
+                            name="email"
+                            className="mt-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                            value={formData.email}
+                            onChange={handleChange}
+                            placeholder="Email"
+                          />
+                        </label>
+                        <label className="mb-4 pr-4 block text-left">
+                          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                            Username
+                          </span>
+                          <input
+                            type="username"
+                            name="username"
+                            className="mt-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                            value={formData.username}
+                            onChange={handleChange}
+                            placeholder="Username"
+                          />
+                        </label>
+                      </div>
+                      <div className="flex flex-row">
+                        <input type="hidden" name="id" value={formData.id} />
+                        <label className="mb-4 pr-4 block text-left">
+                          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+                            Password
+                          </span>
+                          <input
+                            className="mt-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+                            type="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            placeholder="Password"
+                          />
+                        </label>
+                      </div>
+                      <button className="btn btn-warning mt-4" type="submit">
+                        Update Admin
+                      </button>
+                    </li>
+                    <li className="flex flex-col"></li>
+                  </form>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div>
+            <ul></ul>
           </div>
         </div>
-      </div>
-      <div>
-        <ul></ul>
-      </div>
+      )}
     </>
   );
+}
+
+{
+  /* baris-1 */
 }
