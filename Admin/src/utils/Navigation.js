6d04ce399aed0bff4d2a7b99cef01d65be64aac1
Rifky Akhmad F <rifkyakhmad911@gmail.com>
2023-09-25 22:17:41 +0700

    config nav

--- Admin/src/utils/Navigation.js
+++ Admin/src/utils/Navigation.js
@@ -1,5 +1,5 @@
 import { sideNavigation } from "../Config/PagesLink";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const refreshPage = () => {
    const navigate = useNavigate();
@@ -11,3 +11,23 @@ export const allowedPaths = sideNavigation.reduce((accumulator, group) => {
   return accumulator.concat(groupLinks.map((link) => link.href));
 }, []);
 // console.log(allowedPaths);
+
+export const getCurrentUri = () => {
+  const location = useLocation();
+  // console.log(location.pathname);
+  return location.pathname
+}
+
+
+export const getCurrentEndpoint = () => {
+  // Pisahkan string menjadi kata-kata
+  const location = useLocation();
+  // console.log(location.pathname);
+  const routeParts = location.pathname.split("/");
+  const lastPart = routeParts[routeParts.length - 1];
+  // Uppercase huruf pertama
+  const formattedString = lastPart.charAt(0).toUpperCase() + lastPart.slice(1);
+
+  // console.log(formattedString); // Output: "My Profile"
+  return formattedString;
+}
