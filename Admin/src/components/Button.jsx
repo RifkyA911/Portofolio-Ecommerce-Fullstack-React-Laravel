9804a841bdce1d817ba76a9f512b342faddaa264
Rifky Akhmad F <rifkyakhmad911@gmail.com>
2023-10-09 20:54:07 +0700

    restructuring: button component

--- Admin/src/components/Button.jsx
+++ Admin/src/components/Button.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { MuiIcon } from "../utils/RenderIcons";
 
-export const SuccessButton = (props) => {
-  const { message } = props;
+export const ActionButton = (props) => {
+  const { onClickDelete, onClickEdit } = props;
+
+  return (
+    <>
+      <div className="w-full flex lg:flex-row justify-around items-center">
+        <button
+          onClick={onClickDelete}
+          className="p-2 m-2 rounded-md text-gray-500 hover:text-white hover:bg-gradient-to-r hover:from-red-600 hover:to-red-500 hover:outline-none outline outline-2 outline-red-400 transition-all duration-200"
+        >
+          <MuiIcon iconName={"DeleteForeverOutlined"} fontSize={26} />
+        </button>
+        <button
+          onClick={onClickEdit}
+          className="p-2 m-2 rounded-md text-gray-500 hover:text-white hover:bg-gradient-to-r hover:from-blue-600 hover:to-indigo-500 hover:outline-none outline outline-2 outline-blue-400 transition-all duration-200"
+        >
+          <MuiIcon iconName={"AutoFixHighOutlined"} fontSize={26} />
+        </button>
+      </div>
+    </>
+  );
+};
+
+export const MyButton = (props) => {
+  const { className, color, outline } = props;
   // Konten komponen
   return (
     <>
