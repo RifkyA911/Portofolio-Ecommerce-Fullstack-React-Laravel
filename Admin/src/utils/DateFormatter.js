49b4671dc0a2a7c1f28b0a3097545bdbe35d0fec
Rifky Akhmad F <rifkyakhmad911@gmail.com>
2023-10-09 15:10:22 +0700

    add: date formatter

--- Admin/src/utils/DateFormatter.js
+++ Admin/src/utils/DateFormatter.js
@@ -2,4 +2,34 @@ export function convertISODateToJSDate(isoDate) {
     const dateObject = new Date(isoDate);
     return dateObject;
   }
+
+export function DateFormatter(inputDate) {
+
+    // Daftar nama bulan dalam bahasa Indonesia
+    const namaBulan = [
+      "Januari",
+      "Februari",
+      "Maret",
+      "April",
+      "Mei",
+      "Juni",
+      "Juli",
+      "Agustus",
+      "September",
+      "Oktober",
+      "November",
+      "Desember",
+    ];
+
+    const tanggal = new Date(tanggalISO);
+    const tanggalFormatted =
+      tanggal.getDate() +
+      " " +
+      namaBulan[tanggal.getMonth()] +
+      " " +
+      tanggal.getFullYear();
+
+    return tanggalFormatted;
+
+};
   
