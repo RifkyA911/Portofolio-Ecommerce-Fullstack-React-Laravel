184d3fdcd12b65550d0b9f11ffd402032d8f6f18
Rifky Akhmad F <rifkyakhmad911@gmail.com>
2023-11-22 15:47:08 +0700

    add: sort data formatter

--- Admin/src/utils/Formatter.js
+++ Admin/src/utils/Formatter.js
@@ -131,3 +131,28 @@ export function CurrencyFormatter(
     currency: currency,
   }).format(number);
 }
+
+export const SortData = (data, type, sortBy) => {
+  if (!data) {
+    console.error("Data is undefined or null");
+    return { error: "Data is undefined or null", groupedData: {} };
+  }
+
+  if (type === "date") {
+    const sortedData = [...data].sort(
+      (a, b) => new Date(b.updated_at) - new Date(a.updated_at)
+    );
+
+    const groupedData = sortedData.reduce((groups, data) => {
+      const date = data.updated_at.split("T")[0]; // Mengambil bagian tanggal saja
+      groups[date] = [...(groups[date] || []), data];
+      return groups;
+    }, {});
+
+    // Mengembalikan objek dengan data yang diurutkan dan dikelompokkan
+    return groupedData;
+  } else {
+    console.error("Invalid type: " + type);
+    return { error: "Invalid type", groupedData: {} };
+  }
+};
